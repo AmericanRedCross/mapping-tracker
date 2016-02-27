5935d38a39959eb061d0d3dd7cc5d02c2f40b808
danbjoseph <danbjoseph@gmail.com>
2016-02-26 20:57:15 -0500

    fixing line segmentation and db insert

--- routes/Gpx.js
+++ routes/Gpx.js
@@ -98,13 +98,17 @@ Gpx.prototype.segmentTracks = function(cb) {
   var emptyLinestring = function(){
     return turf.linestring([],
     {
-      "date": '',
+      "first": '',
+      "last": '',
       "km": 0,
       "hours": 0,
-      "start": '',
-      "end": '',
       "speed": ''
     })
+  }
+
+  var removeZ = function(latlng){
+    if(latlng.length = 3){ latlng.pop(); }
+    return latlng;
   }
 
   var insertRows = flow.define(
@@ -117,155 +121,153 @@ Gpx.prototype.segmentTracks = function(cb) {
             var data = self.featureCollections[key];
 
             if(data.features){
+              var analyzeLine = function(times, coords){
+                console.log("analyzeLine")
 
-              var addingTo = 0;
-              var holdingArray = [ emptyLinestring(), emptyLinestring() ];
+                var addingTo = 0;
+                var holdingArray = [ emptyLinestring(), emptyLinestring() ];
 
-              var lastTimeStamp;
+                var previousTime;
 
-              var removeZ = function(latlng){
-                if(latlng.length = 3){ latlng.pop(); }
-                return latlng;
-              }
+                holdingArray[0].geometry.coordinates.push(removeZ(coords[0]));
+                holdingArray[0].properties.first = new Date(times[0]);
+                previousTime = new Date(times[0]);
 
-              if(data.features[0].geometry.type === "LineString") {
-                holdingArray[0].geometry.coordinates.push(removeZ(data.features[0].geometry.coordinates[0]));
-                lastTimeStamp = new Date(data.features[0].properties.coordTimes[0]);
-                holdingArray[0].properties.date = lastTimeStamp;
-              } else if (data.features[0].geometry.type === "MultiLineString") {
-                holdingArray[0].geometry.coordinates.push(removeZ(data.features[0].geometry.coordinates[0][0]));
-                lastTimeStamp = new Date(data.features[0].properties.coordTimes[0][0]);
-                holdingArray[0].properties.date = lastTimeStamp;
-              } else {
-                console.log("no LineString or MultiLineString")
-              }
+                for(i=1; i<times.length; i++){
+                  var segmentStart = holdingArray[addingTo].geometry.coordinates[holdingArray[addingTo].geometry.coordinates.length - 1];
+                  var segmentDistance = turf.distance(turf.point(segmentStart), turf.point(removeZ(coords[i])), 'kilometers');
+                  var timeEnd = new Date(times[i]);
+                  var hours = (Math.abs(timeEnd - previousTime) === 0) ? 0 : Math.abs(timeEnd - previousTime) / 3.6e6;
+                  var speed = (segmentDistance > 0 && hours > 0) ? segmentDistance / hours : 0;
+                  var speedCategory = (speed > 6.5) ? 'moto' : 'walk';
 
-              // ### pretend testoutput is the PostGIS database
-              processNextPoint = function(coordinates, datetime){
-                var segmentStart = holdingArray[addingTo].geometry.coordinates[holdingArray[addingTo].geometry.coordinates.length - 1];
-                var segmentDistance = turf.distance(turf.point(segmentStart), turf.point(coordinates), 'kilometers');
-                var timeEnd = new Date(datetime);
-                var hours = (Math.abs(timeEnd - lastTimeStamp) === 0) ? 0 : Math.abs(timeEnd - lastTimeStamp) / 3.6e6;
-                var speed = (segmentDistance > 0 && hours > 0) ? segmentDistance / hours : 0;
-                var speedCategory = (speed > 6.5) ? 'moto' : 'walk';
-                // ## Add to the first line segment until the speed changes
-                if(addingTo === 0){
-                  if(holdingArray[addingTo].geometry.coordinates.length === 1){
+                  if(addingTo === 0 && holdingArray[addingTo].geometry.coordinates.length === 1){
                     holdingArray[addingTo].properties.speed = speedCategory;
                   }
-                  if(holdingArray[addingTo].properties.speed === speedCategory){
-                    //console.log('adding to zero')
-                    holdingArray[addingTo].geometry.coordinates.push(coordinates);
-                    holdingArray[addingTo].properties.km += segmentDistance;
-                    holdingArray[addingTo].properties.hours += hours;
-                    lastTimeStamp = timeEnd;
-                  } else {
-                    // ## been adding to 0, speed switches, start adding to 1
-                    //console.log("switch to 1");
-                    addingTo = 1
-                    holdingArray[addingTo].geometry.coordinates.push(segmentStart);
-                    holdingArray[addingTo].geometry.coordinates.push(coordinates);
-                    holdingArray[addingTo].properties.start = lastTimeStamp;
-                    holdingArray[addingTo].properties.date = lastTimeStamp;
-                    holdingArray[addingTo].properties.speed = speedCategory;
-                    holdingArray[addingTo].properties.km += segmentDistance;
-                    holdingArray[addingTo].properties.hours += hours;
-                    lastTimeStamp = timeEnd;
-                  }
-                } else { // ## adding to 1
-                  if(holdingArray[addingTo].properties.speed === speedCategory){
-                    //console.log('adding to one')
-                    holdingArray[addingTo].geometry.coordinates.push(segmentStart);
-                    holdingArray[addingTo].geometry.coordinates.push(coordinates);
-                    holdingArray[addingTo].properties.speed = speedCategory;
-                    holdingArray[addingTo].properties.km += segmentDistance;
-                    holdingArray[addingTo].properties.hours += hours;
-                    lastTimeStamp = timeEnd;
-                  } else {
-                    // do we think holdingArray 1 should be merged into 0? i.e. it is less than 200m OR shorter than 2 minutes
-                    if(holdingArray[0].properties.speed === holdingArray[1].properties.speed || holdingArray[1].properties.km < 0.2 || holdingArray[1].properties.hours < 0.033 ) {
-                      //console.log('else1')
-                      var slice = holdingArray[1].geometry.coordinates
-                      slice.shift(); // cut off the start coordinate since it is shared by the two, dont want duplicate in the one linestring
-                      holdingArray[0].geometry.coordinates = holdingArray[0].geometry.coordinates.concat(slice)
-                      holdingArray[0].properties.km += holdingArray[1].properties.km;
-                      holdingArray[0].properties.hours += holdingArray[1].properties.hours;
-                      holdingArray.pop()
-                      holdingArray.push(emptyLinestring());
-                      holdingArray[addingTo].geometry.coordinates.push(segmentStart);
-                      holdingArray[addingTo].geometry.coordinates.push(coordinates);
-                      holdingArray[addingTo].properties.date = lastTimeStamp;
-                      holdingArray[addingTo].properties.start = lastTimeStamp;
-                      holdingArray[addingTo].properties.speed = speedCategory;
+                  // ## Add to the first line segment until the speed changes
+                  if(addingTo === 0){
+                    if(holdingArray[addingTo].properties.speed === speedCategory){
+                      //console.log('adding to zero')
+                      holdingArray[addingTo].geometry.coordinates.push(removeZ(coords[i]));
                       holdingArray[addingTo].properties.km += segmentDistance;
                       holdingArray[addingTo].properties.hours += hours;
+                      holdingArray[addingTo].properties.last = timeEnd;
+
+                      previousTime = timeEnd;
                     } else {
-                      //console.log('else2')
-                      // dont merge, shift & ship holdingArray[0]
-                      holdingArray[0].properties.stroke = (holdingArray[0].properties.speed === 'walk') ? "#0047d3" : "#ed1b2e";
-                      holdingArray[0].properties.end = lastTimeStamp;
-                      if (holdingArray[0].properties.hours > 0 ) {
-
-                        sql += "INSERT INTO data.gpx (file,day,first,last,speed,km,hours,geom) VALUES (" +
-                          "'" + key + "'," +
-                          "'" + JSON.stringify(holdingArray[0].properties.date) + "'," +
-                          "'" + JSON.stringify(holdingArray[0].properties.start) + "'," +
-                          "'" + JSON.stringify(holdingArray[0].properties.end) + "'," +
-                          "'" + holdingArray[0].properties.speed + "'," +
-                          holdingArray[0].properties.km + "," +
-                          holdingArray[0].properties.hours + "," +
-                          "ST_GeomFromGeoJSON('{" +
-                          '"type":"LineString","coordinates":' +
-                          JSON.stringify(holdingArray[0].geometry.coordinates) + "," +
-                          '"crs":{"type":"name","properties":{"name":"EPSG:4326"}}}' + "'));";
-
-                      }
-                      holdingArray.shift();
-                      holdingArray.push(emptyLinestring());
+                      // ## been adding to 0, speed switches, start adding to 1
+                      //console.log("switch to 1");
+                      addingTo = 1
                       holdingArray[addingTo].geometry.coordinates.push(segmentStart);
-                      holdingArray[addingTo].geometry.coordinates.push(coordinates);
-                      holdingArray[addingTo].properties.date = lastTimeStamp;
-                      //holdingArray[addingTo].properties.start = lastTimeStamp;
+                      holdingArray[addingTo].geometry.coordinates.push(removeZ(coords[i]));
+                      holdingArray[addingTo].properties.first = previousTime;
+                      holdingArray[addingTo].properties.last = timeEnd;
                       holdingArray[addingTo].properties.speed = speedCategory;
                       holdingArray[addingTo].properties.km += segmentDistance;
                       holdingArray[addingTo].properties.hours += hours;
+                      previousTime = timeEnd;
+                    }
+                  } else { // ## adding to 1
+                    if(holdingArray[addingTo].properties.speed === speedCategory){
+                      //console.log('adding to one')
+                      holdingArray[addingTo].geometry.coordinates.push(removeZ(coords[i]));
+                      holdingArray[addingTo].properties.km += segmentDistance;
+                      holdingArray[addingTo].properties.hours += hours;
+                      holdingArray[addingTo].properties.last = timeEnd;
+                      previousTime = timeEnd;
+                    } else {
+                      // do we think holdingArray 1 should be merged into 0? i.e. it is less than 200m OR shorter than 2 minutes
+                      if(holdingArray[0].properties.speed === holdingArray[1].properties.speed || holdingArray[1].properties.km < 0.2 || holdingArray[1].properties.hours < 0.033 ) {
+                        //console.log('else1')
+                        var slice = holdingArray[1].geometry.coordinates
+                        slice.shift(); // cut off the start coordinate since it is shared by the two, dont want duplicate in the one linestring
+                        holdingArray[0].geometry.coordinates = holdingArray[0].geometry.coordinates.concat(slice)
+                        holdingArray[0].properties.km += holdingArray[1].properties.km;
+                        holdingArray[0].properties.hours += holdingArray[1].properties.hours;
+                        holdingArray[0].properties.last = holdingArray[1].properties.last;
+                        holdingArray.pop()
+                        holdingArray.push(emptyLinestring());
+                        holdingArray[addingTo].geometry.coordinates.push(segmentStart);
+                        holdingArray[addingTo].geometry.coordinates.push(removeZ(coords[i]));
+                        holdingArray[addingTo].properties.first = previousTime;
+                        holdingArray[addingTo].properties.last = timeEnd;
+                        holdingArray[addingTo].properties.speed = speedCategory;
+                        holdingArray[addingTo].properties.km += segmentDistance;
+                        holdingArray[addingTo].properties.hours += hours;
+                        previousTime = timeEnd;
+                      } else {
+                        // console.log('else2')
+                        // dont merge, shift & ship holdingArray[0]
+                        // // holdingArray[0].properties.stroke = (holdingArray[0].properties.speed === 'walk') ? "#0047d3" : "#ed1b2e";
+                        // // holdingArray[0].properties.minutes = holdingArray[0].properties.hours * 60;
+
+                        if (holdingArray[0].properties.hours > 0 ) {
+
+                          // // testOutput.features.push(holdingArray[0]);
+                          sql += "INSERT INTO data.gpx (file,first,last,km,hours,speed,geom) VALUES (" +
+                            "'" + key + "'," +
+                            "'" + JSON.stringify(holdingArray[0].properties.first) + "'," +
+                            "'" + JSON.stringify(holdingArray[0].properties.last) + "'," +
+                            holdingArray[0].properties.km + "," +
+                            holdingArray[0].properties.hours + "," +
+                            "'" + holdingArray[0].properties.speed + "'," +
+                            "ST_GeomFromGeoJSON('{" +
+                            '"type":"LineString","coordinates":' +
+                            JSON.stringify(holdingArray[0].geometry.coordinates) + "," +
+                            '"crs":{"type":"name","properties":{"name":"EPSG:4326"}}}' + "'));";
+
+                        }
+                        holdingArray.shift();
+                        holdingArray.push(emptyLinestring());
+                        holdingArray[addingTo].geometry.coordinates.push(segmentStart);
+                        holdingArray[addingTo].geometry.coordinates.push(removeZ(coords[i]));
+                        holdingArray[addingTo].properties.first = previousTime;
+                        holdingArray[addingTo].properties.last = timeEnd;
+                        holdingArray[addingTo].properties.speed = speedCategory;
+                        holdingArray[addingTo].properties.km += segmentDistance;
+                        holdingArray[addingTo].properties.hours += hours;
+                        previousTime = timeEnd;
+                      }
                     }
                   }
                 }
+
+
+                // // holdingArray[0].properties.stroke = (holdingArray[0].properties.speed === 'walk') ? "#0047d3" : "#ed1b2e";
+                // // holdingArray[0].properties.minutes = holdingArray[0].properties.hours * 60;
+                // // testOutput.features.push(holdingArray[0]);
+                sql += "INSERT INTO data.gpx (file,first,last,km,hours,speed,geom) VALUES (" +
+                  "'" + key + "'," +
+                  "'" + JSON.stringify(holdingArray[0].properties.first) + "'," +
+                  "'" + JSON.stringify(holdingArray[0].properties.last) + "'," +
+                  holdingArray[0].properties.km + "," +
+                  holdingArray[0].properties.hours + "," +
+                  "'" + holdingArray[0].properties.speed + "'," +
+                  "ST_GeomFromGeoJSON('{" +
+                  '"type":"LineString","coordinates":' +
+                  JSON.stringify(holdingArray[0].geometry.coordinates) + "," +
+                  '"crs":{"type":"name","properties":{"name":"EPSG:4326"}}}' + "'));";
+
+
               }
 
-              data.features.forEach(function(element){
-                if(element.geometry.type === "MultiLineString"){
-                  for(i=0; i<element.geometry.coordinates.length; i++){
-                    for(j=0; j<element.geometry.coordinates[i].length; j++){
-                      processNextPoint(removeZ(element.geometry.coordinates[i][j]), element.properties.coordTimes[i][j]);
-                    }
-                  }
+              data.features.forEach(function(feature){
+
+                if(feature.geometry.type === "LineString"){
+                  analyzeLine(feature.properties.coordTimes, feature.geometry.coordinates);
+                } else if (feature.geometry.type === "MultiLineString") {
+                  feature.properties.coordTimes.forEach(function(item, index){
+                    analyzeLine(feature.properties.coordTimes[index], feature.geometry.coordinates[index])
+                  })
+                } else {
+                  console.log(feature.geometry.type + " is not a LineString or MultiLineString")
                 }
 
-                if(element.geometry.type === "LineString"){
-                  for(i=0; i<element.geometry.coordinates.length; i++){
-                    processNextPoint(removeZ(element.geometry.coordinates[i]), element.properties.coordTimes[i]);
-                  }
-                }
               });
 
-              holdingArray[0].properties.end = lastTimeStamp;
-              holdingArray[0].properties.stroke = (holdingArray[0].properties.speed === 'walk') ? "#0047d3" : "#ed1b2e";
 
+              // // fs.writeFile("./segmentation-output-3.geojson", JSON.stringify(testOutput))
 
-              sql += "INSERT INTO data.gpx (file,day,first,last,speed,km,hours,geom) VALUES (" +
-                "'" + key + "'," +
-                "'" + JSON.stringify(holdingArray[0].properties.date) + "'," +
-                "'" + JSON.stringify(holdingArray[0].properties.start) + "'," +
-                "'" + JSON.stringify(holdingArray[0].properties.end) + "'," +
-                "'" + holdingArray[0].properties.speed + "'," +
-                holdingArray[0].properties.km + "," +
-                holdingArray[0].properties.hours + "," +
-                "ST_GeomFromGeoJSON('{" +
-                '"type":"LineString","coordinates":' +
-                JSON.stringify(holdingArray[0].geometry.coordinates) + "," +
-                '"crs":{"type":"name","properties":{"name":"EPSG:4326"}}}' + "'));";
 
               pghelper.query(sql, this.MULTI());
             }
